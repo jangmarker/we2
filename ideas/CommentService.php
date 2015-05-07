3469ef2be2c3a8b34d4273a04d6ed4098c9e0e78
Jan Marker <jan@jangmarker.de>
2015-05-07 20:19:05 +0200

    Insert aspects and related ideas based on comment

--- ideas/CommentService.php
+++ ideas/CommentService.php
@@ -22,7 +22,46 @@ class CommentService extends \framework\Service {
         $stm->bindValue(':idea_id', $data['ideaId']);
         $stm->execute();
 
+        $newCommentId = $db->lastInsertId();
+
+        $this->extractAndInsertIdeaReferences($newCommentId, $data['comment']);
+        $this->extractAndInsertAspects($newCommentId, $data['comment']);
+
         return array('success' => 'true');
+    }
+
+    private function extractAndInsertIdeaReferences($newCommentId, $comment) {
+        $matches = array();
+        if (preg_match_all("/idea ([0-9]+)/", $comment, $matches)) {
+            $this->db()->beginTransaction();
+            foreach ($matches[1] as $ideaId) {
+                $stm = $this->db()->prepare("
+                    INSERT INTO related_ideas (commentid, idea_id)
+                    VALUES(:commentid, :idea_id)
+                ");
+                $stm->bindValue(":commentid", $newCommentId, PDO::PARAM_INT);
+                $stm->bindValue(":idea_id", $ideaId, PDO::PARAM_INT);
+                $stm->execute();
+            }
+            $this->db()->commit();
+        }
+    }
+
+    private function extractAndInsertAspects($newCommentId, $comment) {
+        $matches = array();
+        if (preg_match_all("/#[a-zA-Z][a-zA-Z0-9]*/", $comment, $matches)) {
+            $this->db()->beginTransaction();
+            foreach ($matches[0] as $aspect) {
+                $stm = $this->db()->prepare("
+                    INSERT INTO aspects (commentid, aspectname)
+                    VALUES(:commentid, :aspectname)
+                ");
+                $stm->bindValue(":commentid", $newCommentId, PDO::PARAM_INT);
+                $stm->bindValue(":aspectname", $aspect, PDO::PARAM_STR);
+                $stm->execute();
+            }
+            $this->db()->commit();
+        }
     }
 
 }
